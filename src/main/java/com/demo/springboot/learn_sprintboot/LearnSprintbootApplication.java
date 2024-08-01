bdba0efcb4109641d304634f195d680d850b5664
CherylShi127 <shi.qin@northeastern.edu>
2024-08-01 14:46:44 -0400

    commit

--- src/main/java/com/demo/springboot/learn_sprintboot/LearnSprintbootApplication.java
+++ src/main/java/com/demo/springboot/learn_sprintboot/LearnSprintbootApplication.java
@@ -2,9 +2,16 @@ package com.demo.springboot.learn_sprintboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LearnSprintbootApplication {
+	@RequestMapping("/")
+	public String home(){
+		return "Dockerizing Spring Boot Application";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSprintbootApplication.class, args);
